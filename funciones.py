dc95807807940c466f3ec15e85c968d5da99d760
SantRR <192172-7@iberoleon.edu.mx>
2023-11-09 09:24:59 -0600

    fix 1
    -la función determinante ya hace lo que debe
    -las funciones ahora están en PascalCase

---
+++ funciones.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def PedirMatriz(filas, columnas):
+    matriz = []
+    for i in range(filas):
+        fila = []
+        for j in range(columnas):
+            elemento = float(input(f"Ingrese el valor para la fila {i+1}, columna {j+1}: "))
+            fila.append(elemento)
+        matriz.append(fila)
+    matriz_np = np.array(matriz)   
+    return matriz_np
+
+def Determinante(matriz):
+    determinante = np.linalg.det(matriz)
+    determinante = np.array(determinante)
+    return determinante
+
+def ProductoEscalar(escalar, matriz):
+    matriz *= escalar
+    matriz_escalar = np.array(matriz) 
+    return matriz_escalar
+
+def SumaMatrices(matriz1, matriz2):
+    suma = matriz1 + matriz2
+    return suma
+    
+def RestaMatrices(matriz1, matriz2):
+    resta = matriz1 - matriz2
+    return resta
